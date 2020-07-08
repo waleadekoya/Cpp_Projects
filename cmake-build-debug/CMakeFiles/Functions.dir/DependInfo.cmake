2dddf48b6bf96aabe6396e29af05d1156034ef48
Wale Adekoya <adekoya.wale@yahoo.com>
2020-07-08 09:10:02 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Functions.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Sesan/OneDrive/@C++/Functions/StrorageClass.cpp" "C:/Users/Sesan/OneDrive/@C++/Functions/cmake-build-debug/CMakeFiles/Functions.dir/StrorageClass.cpp.obj"
+  "C:/Users/Sesan/OneDrive/@C++/Functions/functions.cpp" "C:/Users/Sesan/OneDrive/@C++/Functions/cmake-build-debug/CMakeFiles/Functions.dir/functions.cpp.obj"
+  "C:/Users/Sesan/OneDrive/@C++/Functions/main.cpp" "C:/Users/Sesan/OneDrive/@C++/Functions/cmake-build-debug/CMakeFiles/Functions.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Functions.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Functions.dir/StrorageClass.cpp.obj"
+  "CMakeFiles/Functions.dir/functions.cpp.obj"
+  "CMakeFiles/Functions.dir/main.cpp.obj"
+  "Functions.exe"
+  "Functions.exe.manifest"
+  "Functions.pdb"
+  "libFunctions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
